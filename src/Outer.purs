84c3b04a72d4d3c8427c17268649884b95d33bcf
YueKaburagi <class_mercurycc@yahoo.co.jp>
2017-01-24 14:26:07 +0900

    remove obsoleted code

--- src/Outer.purs
+++ src/Outer.purs
@@ -72,30 +72,6 @@ approach jar =
   ChildProcess.spawn "java" ["-jar", jar, "-b"] defaultSpawnOptions
 
 
-textQuery :: forall eff . ChildProcess -> String -> Aff _ (Maybe String)
-textQuery p q = do
-  void $ liftEff $ readAsBuffer p -- Prompt などを読み捨て
-  writeQueryAff p q
-  s <- readOnceAff p
-  liftEff $ log $ show s  
-  pure (dropLastPrompt =<< s)
-
-jsonQuery :: forall eff . ChildProcess -> String -> Aff _ Json
-jsonQuery p q = do
-  s <- textQuery p q
-  pass $ maybe (Left "empty json") jsonParser s
-  where
-    pass (Left s) = liftEff <<< throwException <<< error $ s
-    pass (Right json) = pure json
-
--- translate は optional にしたいので、追加で Boolean を引数に入れるかも
-rawQuery :: forall eff . ChildProcess -> String -> Aff _ (Maybe String)
-rawQuery p ix = textQuery p ("find #" <> ix <> " forFacadeRaw")
-listQuery :: forall eff . ChildProcess -> Array String -> Aff _ Json
-listQuery p q = jsonQuery p ((String.joinWith " " q) <> " forFacadeList translate")
-infoQuery :: forall eff . ChildProcess -> String -> Aff _ Json
-infoQuery p ix = jsonQuery p ("find #" <> ix <> " forFacadeInfo translate")
-
 
 
 writeQuery :: forall eff
@@ -113,25 +89,6 @@ writeQueryAff :: forall eff
              -> String
              -> Aff ("cp" :: CHILD_PROCESS, "console" :: CONSOLE | eff ) Unit
 writeQueryAff p q = makeAff \errCb cb -> writeQuery p q cb
-
-readAsBuffer :: forall eff
-                . ChildProcess
-                -> Eff ("cp" :: CHILD_PROCESS, "err" :: EXCEPTION | eff) (Maybe Buffer)
-readAsBuffer p = do
-  Stream.read (ChildProcess.stdout p) Nothing
-
-dropLastPrompt :: String -> Maybe String
-dropLastPrompt str = String.joinWith "\n" <$> (abc $ String.split (Pattern "\n") str)
-  where
-    abc s = dropPrompt s (Array.unsnoc s)
-    dropPrompt s (Just ss) =
-      case ss.last of
-        "Browser > " ->
-          case ss.init of
-            [] -> Nothing
-            _ -> Just ss.init
-        _ -> Just s
-    dropPrompt _ Nothing = Nothing
 
 -- -- --
 
@@ -190,8 +147,11 @@ readyPrompt = do
     true -> pure unit
     false -> do
       e <- readOnceP
-      liftEff <<< log <<< ("warn" <> _) <<< show $ e
+      warnJust e
       readyPrompt
+  where
+    warnJust (Just s) = liftEff $ log $ ("[warn]" <> s)
+    warnJust Nothing = pure unit
 
 textQueryP :: forall eff .  String -> Prompt _ (Maybe String)
 textQueryP q = do
@@ -199,7 +159,6 @@ textQueryP q = do
   readyPrompt
   lift $ writeQueryAff p q
   s <- readOnceP
---  liftEff $ log $ show s  
   pure s
 
 jsonQueryP :: forall eff . String -> Prompt _ Json
@@ -218,26 +177,3 @@ listQueryP q = jsonQueryP ((String.joinWith " " q) <> " forFacadeList translate"
 infoQueryP :: forall eff . String -> Prompt _ Json
 infoQueryP ix = jsonQueryP ("find #" <> ix <> " forFacadeInfo translate")
 
-
-
-
--- deprecated
-waitPrompt :: forall eff
-              . ChildProcess
-              -> Aff ("cp" :: CHILD_PROCESS, "err" :: EXCEPTION | eff) Unit
-waitPrompt p = do
-    str <- liftEff $ Stream.readString src Nothing UTF8
-    wait str
-  where
-    src = ChildProcess.stdout p
-    wait :: Maybe String -> Aff _ Unit
-    wait ms = do
-      b <- liftEff $ isPrompt ms
-      repbf b
-    isPrompt (Just "Browser > ") = pure true
-    isPrompt Nothing = pure false
-    isPrompt (Just failed) = throwException $ error failed -- 読んじゃいけないもの読んだ
-    repbf true = pure unit
-    repbf false = do
-      str <- readOnceAff p
-      wait str
